823788895dd7dd7bf2a7a890615e24d800841592
Jayana Nanayakkara <jchdnjc@gmail.com>
2024-03-07 19:19:02 -0800

    First Commit

--- src/App_Chart.js
+++ src/App_Chart.js
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Form, Button, AutoComplete, Card, Alert, Table, Spin } from "antd";
+// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+// import { Pie } from 'react-chartjs-2';
 import Banner from "./Banner";
 import "./styles/App.css";
 
+// ChartJS.register(ArcElement, Tooltip, Legend);
 require('dotenv').config();
-POLYGON_KEY = process.env.POLYGON_KEY;
 
 function App() {
   const [originalArray, setOriginalArray] = useState(Array(10).fill(''));
   const [error, setError] = useState('');
   const [showAlert, setShowAlert] = useState(false);
   const [hrpWeights, setHrpWeights] = useState(null);
+  // const [chartData, setChartData] = useState(null);
   const [dataSource, setDataSource] = useState([]);
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(false);
-  const [logoUrls, setLogoUrls] = useState({}); 
+  const [logoUrls, setLogoUrls] = useState({}); // State to store logo URLs for ticker symbols
 
   const handleChange = (index, value) => {
     const newOriginalArray = [...originalArray];
@@ -65,13 +68,36 @@ function App() {
       const hrpWeights = response.data.hrp_weights;
       setHrpWeights(hrpWeights);
   
+      // const labels = originalArray.filter(symbol => symbol.trim() !== '');
+      
+      // const chartData = {
+      //   labels: labels,
+      //   datasets: [
+      //     {
+      //       data: hrpWeights,
+      //       backgroundColor: [
+      //         "rgba(183, 58, 84, 1)",
+      //         "rgba(58, 84, 183, 1)",
+      //         "rgba(172, 0, 193, 1)",
+      //         "rgba(193, 0, 172, 1)",
+      //         "rgba(183, 84, 58, 1)",
+      //         "rgba(58, 183, 84, 1)",
+      //         "rgba(84, 183, 58, 1)",
+      //         "rgba(84, 58, 183, 1)",
+      //         "rgba(0, 172, 193, 1)"
+      //       ],
+      //     },
+      //   ],
+      // };
+      // setChartData(chartData);
+  
       const logoPromises = originalArray.map(async symbol => {
         try {
           const response = await fetch(
-            `https://api.polygon.io/v3/reference/tickers/${symbol}?apiKey=${POLYGON_KEY}`
+            `https://api.polygon.io/v3/reference/tickers/${symbol}?apiKey=CPgjfwDJOutj46KdeJhwtHC2UfQL5Ble`
           );
           const data = await response.json();
-          const logoUrl = `${data?.results?.branding?.icon_url}?apiKey=${POLYGON_KEY}`;
+          const logoUrl = `${data?.results?.branding?.icon_url}?apiKey=CPgjfwDJOutj46KdeJhwtHC2UfQL5Ble`;
           return { symbol, logoUrl };
         } catch (error) {
           console.error('Error fetching logo:', error);
@@ -146,6 +172,33 @@ function App() {
                 /></div>
             }
 
+            {/* {chartData && (
+              <div style={{ marginTop: '20px', width: '50%', margin: '0 auto' }}>
+                <Pie 
+                  data={chartData} 
+                  options={{
+                    responsive: true,
+                    maintainAspectRatio: true,
+                    plugins: {
+                      legend: {
+                        display: true,
+                        position: 'bottom',
+                      },
+                    },
+                    tooltips: {
+                      callbacks: {
+                        label: (tooltipItem, data) => {
+                          const label = data.labels[tooltipItem.index];
+                          const value = data.datasets[0].data[tooltipItem.index];
+                          return `${label}: ${(value * 100).toFixed(1)}%`;
+                        },
+                      },
+                    },
+                  }}
+                />
+              </div>
+            )} */}
+
             {hrpWeights && (
               <div style={{ marginTop: '20px' }}>
                 <h2>Hierarchical Risk Parity Weights</h2>
@@ -183,6 +236,7 @@ function App() {
         </div>
       </div>
 
+      {/* Table with company logos */}
       
 
     </div>
